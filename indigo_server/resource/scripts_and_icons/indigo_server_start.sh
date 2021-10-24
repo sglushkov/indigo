948d3677d8749a7e733c205f9509648838e2271d
sglushkov <stoyan.glushkov@gmail.com>
2021-10-24 15:24:17 +0300

    updated start script to use home directory

--- indigo_server/resource/scripts_and_icons/indigo_server_start.sh
+++ indigo_server/resource/scripts_and_icons/indigo_server_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall indigo_server
-/home/tony/indigo/build/bin/indigo_server indigo_ccd_asi indigo_mount_nexstar indigo_focuser_mypro2_tony indigo_wheel_qhy
+~/indigo/build/bin/indigo_server indigo_ccd_asi indigo_mount_nexstar indigo_focuser_mypro2_tony indigo_wheel_qhy
 
 
 
